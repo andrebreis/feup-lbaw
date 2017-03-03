73fa55c42815009af0146deb8151789214f72652
Francisca96 <franciscapauperio@gmail.com>
2017-03-03 09:55:22 +0000

    Profile identity card

--- src/pages/profile.php
+++ src/pages/profile.php
@@ -4,7 +4,25 @@ include_once "header.html";
 
 <h1>Profile</h1>
 
-<img src="../img/profile-pic.png" alt="Profile picture" style="width:250px;height:250px;">
+<div class="container">
+    <div class="col-xs-8 col-md-3">
+        <img src="../img/profile-pic.png" alt="Profile picture" style="width:17em;height:17em;">
+    </div>
+
+    <div class="panel panel-default col-xs-8 col-md-5">
+        <!-- Default panel contents -->
+        <div class="panel-heading">Francisca Paupério</div>
+
+        <!-- List group -->
+        <ul class="list-group">
+            <li class="list-group-item">Student</li>
+            <li class="list-group-item">FEUP</li>
+            <li class="list-group-item">Porto, Portugal</li>
+            <li class="list-group-item">franciscapauperio@gmail.com</li>
+
+        </ul>
+    </div>
+</div>
 
 <?php
 include_once "footer.html";
