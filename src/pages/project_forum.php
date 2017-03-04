6eba0a692096eba66fca91f1e49ee70388a208d0
Andre <andrebrreis@gmail.com>
2017-03-04 23:46:40 +0000

    Fix project forum

--- src/pages/project_forum.php
+++ src/pages/project_forum.php
@@ -7,16 +7,7 @@ include_once "project_navbar.html";
 ?>
 
 <div class="wrapper col-md-8">
-  <h1>Settings</h1>
-  <select>
-    <div class="dropdown">
-      <button class="btn btn-default dropdown-toggle" type="button" id="project-visibility" data-toggle="dropdown">Project Visibility
-      <span class="caret"></span></button>
-      <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
-        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Public</a></li>
-        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Private</a></li>
-      </ul>
-      </div>  <button type="button" class="btn btn-danger">Delete Project</button>
+  <h1>Project Forum</h1>
 </div>
 
 <?php
