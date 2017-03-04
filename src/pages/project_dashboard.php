8efaab1d4e83b5a336e82ecf520161d44d47caf3
Andre <andrebrreis@gmail.com>
2017-03-04 17:37:01 +0000

    Add project dashboard page

---
+++ src/pages/project_dashboard.php
@@ -0,0 +1,32 @@
+<?php
+include_once "header.html";
+?>
+
+<div class="navbar navbar-default navbar-static-top" id="sidebar">
+  <ul class="nav navbar-nav">
+
+    <li><a href="project.php" title="back to my projects"><i class="fa fa-arrow-left" aria-hidden="true"></i></a></li>
+    <li><a href="project_dashboard.php" title="project dashboard"><i class="fa fa-briefcase" aria-hidden="true"></i></a></li>
+    <li><a href="collaborators.php" title="project collaborators"><i class="fa fa-users" aria-hidden="true"></i></a></li>
+    <li><a href="tasks.php" title="tasks"><i class="fa fa-tasks" aria-hidden="true"></i></i></a></li>
+    <li><a href="milestones.php" title="milestones"><i class="fa fa-calendar" aria-hidden="true"></i></a></li>
+    <li><a href="statistics.php" title="statistics"><i class="fa fa-line-chart" aria-hidden="true"></i></a></li>
+
+  </ul>
+</div>
+
+
+<h1>Project Dashboard</h1>
+
+<h2>Project Name</h2>
+
+<div class="col-md-8" id="project-description">
+<p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus malesuada dictum nulla et semper. Curabitur eu lacus eu eros egestas tempus in a augue. Etiam dictum sed urna sit amet maximus. Phasellus lobortis nibh ut nunc rhoncus, tristique condimentum libero semper. Suspendisse tempus gravida diam eleifend porta. Nam gravida mi id nulla posuere, condimentum pulvinar lacus rutrum. Fusce tempor sapien ac est pretium, ac fringilla mauris efficitur. Pellentesque volutpat ullamcorper massa eu aliquet. Sed mollis commodo sapien et scelerisque. Donec malesuada et mi in pharetra. Morbi dapibus neque ac nisi tempor, nec facilisis libero accumsan. Ut venenatis accumsan mollis.
+</p>
+</div>
+
+
+<?php
+include_once "footer.html";
+?>
