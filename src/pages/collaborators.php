3d1d618988c6be7060ccbfd839f18aa9630a40ce
Andre <andrebrreis@gmail.com>
2017-03-04 19:19:18 +0000

    finish collaborators page stub

---
+++ src/pages/collaborators.php
@@ -0,0 +1,38 @@
+<?php
+include_once "header.html";
+?>
+
+<?php
+include_once "project_navbar.html";
+?>
+
+<h1>Collaborators</h1>
+
+<div class="table-responsive">
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th>Username</th>
+        <th>Role</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>andre</td> <td>Coordinator</td>
+      </tr>
+      <tr>
+        <td>pik42</td> <td>Coordinator</td>
+      </tr>
+      <tr>
+        <td>semi1</td> <td>Collaborator</td>
+      </tr>
+      <tr>
+        <td>semi3</td> <td>Collaborator</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+<?php
+include_once "footer.html";
+?>
