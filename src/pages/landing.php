f14a5912394bcd3ee0f2bb340b046af46985eecb
Andre <andrebrreis@gmail.com>
2017-03-05 19:19:31 +0000

    Add registration/login modal

--- src/pages/landing.php
+++ src/pages/landing.php
@@ -29,18 +29,137 @@
 <div class="container">
     <img id="logo" src="../assets/logo.png">
     <div id="login">
-        <div id="credentials">
-            <input type="text" name="username" placeholder="Username">
-            <input type="text" name="password" placeholder="Password">
-        </div>
-
-        <div id="buttons">
-            <br><input type="submit" class="btn-login" name="submit" value="Login">
-            <input type="submit" class="btn-google" name="submit" value="Login with Google">
-            <br><br><a href="register.php">Register</a><br>
-        </div>
+        <button type="button" class="btn btn-default" data-toggle="modal" data-target="#myModal">Sign In/Up</button>
     </div>
     <input id="search-bar" class="col-md-4 col-md-offset-5" type="text" name="search" placeholder="Search...">
+</div>
+  </div>
+
+  <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
+  aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+          <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+                  ×</button>
+              <h4 class="modal-title" id="myModalLabel">
+                  Login/Registration</h4>
+          </div>
+          <div class="modal-body">
+              <div class="row">
+                  <div class="col-md-8" style="border-right: 1px dotted #C2C2C2;padding-right: 30px;">
+                      <!-- Nav tabs -->
+                      <ul class="nav nav-tabs">
+                          <li class="active"><a href="#Login" data-toggle="tab">Login</a></li>
+                          <li><a href="#Registration" data-toggle="tab">Registration</a></li>
+                      </ul>
+                      <!-- Tab panes -->
+                      <div class="tab-content">
+                          <div class="tab-pane active" id="Login">
+                              <form role="form" class="form-horizontal">
+                              <div class="form-group">
+                                  <label for="username" class="col-sm-2 control-label">
+                                      Username</label>
+                                  <div class="col-sm-10">
+                                      <input type="text" class="form-control" id="username1" placeholder="Username" />
+                                  </div>
+                              </div>
+                              <div class="form-group">
+                                  <label for="exampleInputPassword1" class="col-sm-2 control-label">
+                                      Password</label>
+                                  <div class="col-sm-10">
+                                      <input type="email" class="form-control" id="exampleInputPassword1" placeholder="Email" />
+                                  </div>
+                              </div>
+                              <div class="row">
+                                  <div class="col-sm-2">
+                                  </div>
+                                  <div class="col-sm-10">
+                                      <button type="submit" class="btn btn-primary btn-sm">
+                                          Submit</button>
+                                  </div>
+                              </div>
+                              </form>
+                          </div>
+                          <div class="tab-pane" id="Registration">
+                              <form role="form" class="form-horizontal">
+                              <div class="form-group">
+                                  <label for="username" class="col-sm-2 control-label">
+                                      Username</label>
+                                  <div class="col-sm-10">
+                                      <div class="row">
+                                          <div class="col-md-9">
+                                              <input type="text" class="form-control" placeholder="Name" />
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                              <div class="form-group">
+                                  <label for="name" class="col-sm-2 control-label">
+                                      Name</label>
+                                  <div class="col-sm-10">
+                                      <div class="row">
+                                          <div class="col-md-9">
+                                              <input type="text" class="form-control" placeholder="Name" />
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                              <div class="form-group">
+                                  <label for="email" class="col-sm-2 control-label">
+                                      Email</label>
+                                  <div class="col-sm-10">
+                                      <input type="email" class="form-control" id="email" placeholder="Email" />
+                                  </div>
+                              </div>
+                              <div class="form-group">
+                                  <label for="password" class="col-sm-2 control-label">
+                                      Password</label>
+                                  <div class="col-sm-10">
+                                      <input type="password" class="form-control" id="password" placeholder="Password" />
+                                  </div>
+                              </div>
+                              <div class="form-group">
+                                  <label for="repeat-password" class="col-sm-2 control-label">
+                                      Repeat Password</label>
+                                  <div class="col-sm-10">
+                                      <input type="password" class="form-control" id="repeat-password" placeholder="Repeat Password" />
+                                  </div>
+                              </div>
+                              <div class="row">
+                                  <div class="col-sm-2">
+                                  </div>
+                                  <div class="col-sm-10">
+                                      <button type="button" class="btn btn-primary btn-sm">
+                                          Save & Continue</button>
+                                      <button type="button" class="btn btn-default btn-sm">
+                                          Cancel</button>
+                                  </div>
+                              </div>
+                              </form>
+                          </div>
+                      </div>
+                      <div id="OR" class="hidden-xs">
+                          OR</div>
+                  </div>
+                  <div class="col-md-4">
+                      <div class="row text-center sign-with">
+                          <div class="col-md-12">
+                              <h3>
+                                  Sign in with</h3>
+                          </div>
+                          <div class="col-md-12">
+                              <div class="btn-group btn-group-justified">
+                                  <a href="#" class="btn btn-danger">
+                                      Google</a>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
 </div>
 </body>
 </html>
