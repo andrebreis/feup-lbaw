36219b44608d0837b6fb8f8811092238753dfb89
zecst19 <zecst19@gmail.com>
2017-03-05 12:27:22 +0000

    project forum page and rounded corners on header

--- src/pages/project_forum.php
+++ src/pages/project_forum.php
@@ -1,3 +1,30 @@
+	<!doctype html>
+
+	<html lang="en">
+	<head>
+		<meta charset="utf-8">
+
+		<title>Search Results</title>
+		<meta name="description" content="The HTML5 Herald">
+		<meta name="author" content="SitePoint">
+
+		<!-- Bootstrap -->
+		<link href="../css/bootstrap.min.css" rel="stylesheet">
+		<link href="../css/font-awesome.min.css" rel="stylesheet">
+
+		<link href="../css/projectForum.css" rel="stylesheet">
+		<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+
+		
+
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="../js/bootstrap.min.js"></script>
+
+	</head>
+<body>
 <?php
 include_once "header.html";
 ?>
@@ -7,9 +34,141 @@ include_once "project_navbar.html";
 ?>
 
 <div class="wrapper col-md-8">
-  <h1>Project Forum</h1>
+  
+  <div id="forum-items" class="col-md-12">
+	<div class="container">
+			<div class="row">
+
+				<section class="content">
+				
+						<div class="panel panel-default">
+
+							<div class="panel-body">
+							
+								<div class="pull-left">					
+									<h1>Project Forum</h1>
+									
+								</div>								
+								<div class="table-container">
+									<table class="table table-filter">
+										<tbody>
+											<tr data-status="user">
+												<td>
+													<div class="media">
+														<a href="#" class="pull-left">
+															<img src="https://s3.amazonaws.com/uifaces/faces/twitter/fffabs/128.jpg" class="media-photo">
+														</a>
+														<div class="media-body">
+															<span class="media-meta pull-right">Febrero 13, 2016</span>
+															<h4 class="title">
+																Lorem Impsum
+															</h4>
+															<span class="badge badge-default badge-pill pull-right">2</span>
+															<p class="summary">Ut enim ad minim veniam, quis nostrud exercitation...</p>
+														</div>
+													</div>
+												</td>
+											</tr>
+											<tr data-status="user">
+												<td>
+													<div class="media">
+														<a href="#" class="pull-left">
+															<img src="https://s3.amazonaws.com/uifaces/faces/twitter/fffabs/128.jpg" class="media-photo">
+														</a>
+														<div class="media-body">
+															<span class="media-meta pull-right">Febrero 13, 2016</span>
+															<h4 class="title">
+																Lorem Impsum
+															</h4>
+															<p class="summary">Ut enim ad minim veniam, quis nostrud exercitation...</p>
+														</div>
+													</div>
+												</td>
+											</tr>
+											<tr data-status="project">
+												<td>
+													<div class="media">
+														<a href="#" class="pull-left">
+															<img src="https://s3.amazonaws.com/uifaces/faces/twitter/fffabs/128.jpg" class="media-photo">
+														</a>
+														<div class="media-body">
+															<span class="media-meta pull-right">Febrero 13, 2016</span>
+															<h4 class="title">
+																Lorem Impsum
+															</h4>
+															<span class="badge badge-default badge-pill pull-right">1</span>
+															<p class="summary">Ut enim ad minim veniam, quis nostrud exercitation...</p>
+														</div>
+													</div>
+												</td>
+											</tr>
+											<tr data-status="project">
+												<td>
+													<div class="media">
+														<a href="#" class="pull-left">
+															<img src="https://s3.amazonaws.com/uifaces/faces/twitter/fffabs/128.jpg" class="media-photo">
+														</a>
+														<div class="media-body">
+															<span class="media-meta pull-right">Febrero 13, 2016</span>
+															<h4 class="title">
+																Lorem Impsum
+															</h4>
+															<span class="badge badge-default badge-pill pull-right">6</span>
+															<p class="summary">Ut enim ad minim veniam, quis nostrud exercitation...</p>
+														</div>
+													</div>
+												</td>
+											</tr>
+											<tr data-status="user">
+												<td>
+													<div class="media">
+														<a href="#" class="pull-left">
+															<img src="https://s3.amazonaws.com/uifaces/faces/twitter/fffabs/128.jpg" class="media-photo">
+														</a>
+														<div class="media-body">
+															<span class="media-meta pull-right">Febrero 13, 2016</span>
+															<h4 class="title">
+																Lorem Impsum
+															</h4>
+															<span class="badge badge-default badge-pill pull-right">14</span>
+															<p class="summary">Ut enim ad minim veniam, quis nostrud exercitation...</p>
+														</div>
+													</div>
+												</td>
+											</tr>
+											<tr data-status="project">
+												<td>
+													<div class="media">
+														<a href="#" class="pull-left">
+															<img src="https://s3.amazonaws.com/uifaces/faces/twitter/fffabs/128.jpg" class="media-photo">
+														</a>
+														<div class="media-body">
+															<span class="media-meta pull-right">Febrero 13, 2016</span>
+															<h4 class="title">
+																Lorem Impsum
+																
+															</h4>
+															<span class="badge badge-default badge-pill pull-right">14</span>
+															<p class="summary">Ut enim ad minim veniam, quis nostrud exercitation...</p>
+															
+														</div>
+													</div>
+												</td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+							</div>
+						
+					</section>
+
+				</div>
+			</div>
+  </div>
 </div>
 
 <?php
 include_once "footer.html";
 ?>
+</body>
+</html>
