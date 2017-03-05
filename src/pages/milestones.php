f2bdaf1cb896a2be9ee1fd10200efdede4129abf
zecst19 <zecst19@gmail.com>
2017-03-05 18:03:03 +0000

    milestones page

--- src/pages/milestones.php
+++ src/pages/milestones.php
@@ -1,3 +1,29 @@
+<!doctype html>
+
+	<html lang="en">
+	<head>
+		<meta charset="utf-8">
+
+		<title>Milestones</title>
+		<meta name="description" content="The HTML5 Herald">
+		<meta name="author" content="SitePoint">
+
+		<!-- Bootstrap -->
+		<link href="../css/bootstrap.min.css" rel="stylesheet">
+		<link href="../css/font-awesome.min.css" rel="stylesheet">
+
+		<link href="../css/milestones.css" rel="stylesheet">
+		<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+
+		
+
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="../js/bootstrap.min.js"></script>
+	</head>
+<body>
 <?php
 include_once "header.html";
 ?>
@@ -7,9 +33,151 @@ include_once "project_navbar.html";
 ?>
 
 <div class="wrapper col-md-8">
-  <h1>Milestones</h1>
+  
+  <div id="forum-items" class="col-md-12">
+	<div class="container">
+			<div class="row">
+
+				<section class="content">
+				
+						<div class="panel panel-default">
+
+							<div class="panel-body">
+							
+								<div class="pull-left">
+									<h1>Milestones</h1>
+								</div>
+								<div class="col-md-12">
+									<button id="create-task" type="button" class="btn btn-success col-md-offset-10 col-md-2 pull-right">Create Milestone</button>
+								</div>
+								
+								<div class="table-container">
+									<table class="table table-filter">
+										<tbody>
+											<tr>
+												<td>
+													<div class="media">													
+														<div class="media-body">
+															<span class="media-meta pull-right"><small>Deadline: </small><b>Febrero 13, 2016</b> </span>
+															<h4 class="title">
+																Lorem Impsum
+															</h4>
+															<div class="progress">
+																<span class="progress-value">80% - Under Development</span>
+																<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="80"
+																	aria-valuemin="0" aria-valuemax="100" style="width:80%">
+																</div>
+															</div>
+														</div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="media">
+														<div class="media-body">
+															<span class="media-meta pull-right"><small>Deadline: </small><b>Febrero 13, 2016</b> </span>
+															<h4 class="title">
+																Lorem Impsum
+															</h4>
+															<div class="progress">
+																<span class="progress-value">100% - Complete</span>
+																<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="100"
+																	aria-valuemin="0" aria-valuemax="100" style="width:100%">
+																</div>
+															</div>
+														</div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="media">
+														<div class="media-body">
+															<span class="media-meta pull-right"><small>Deadline: </small><b>Febrero 13, 2016</b> </span>
+															<h4 class="title">
+																Lorem Impsum
+															</h4>
+															<div class="progress">
+																<span class="progress-value">40% - Under Development</span>
+																<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40"
+																	aria-valuemin="0" aria-valuemax="100" style="width:40%">
+																</div>
+															</div>
+														</div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="media">
+														<div class="media-body">
+															<span class="media-meta pull-right"><small>Deadline: </small><b>Febrero 13, 2016</b> </span>
+															<h4 class="title">
+																Lorem Impsum
+															</h4>
+															<div class="progress">
+																<span class="progress-value">10% - Paused</span>
+																<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="10"
+																	aria-valuemin="0" aria-valuemax="100" style="width:10%">
+																</div>
+															</div>
+														</div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="media">
+														<div class="media-body">
+															<span class="media-meta pull-right"><small>Deadline: </small><b>Febrero 13, 2016</b> </span>
+															<h4 class="title">
+																Lorem Impsum
+															</h4>
+															<div class="progress">
+																<span class="progress-value">0% - Pending Development</span>
+																<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="0"
+																	aria-valuemin="0" aria-valuemax="100" style="width:0%">
+																</div>
+															</div>
+														</div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="media">
+														<div class="media-body">
+															<span class="media-meta pull-right"><small>Deadline: </small><b>Febrero 13, 2016</b> </span>
+															<h4 class="title">
+																Lorem Impsum
+																
+															</h4>
+															<div class="progress">
+																<span class="progress-value">40% - Under Development</span>
+																<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40"
+																	aria-valuemin="0" aria-valuemax="100" style="width:40%">
+																</div>
+															</div>
+															
+														</div>
+													</div>
+												</td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+							</div>
+						
+					</section>
+
+				</div>
+			</div>
+  </div>
 </div>
 
 <?php
 include_once "footer.html";
 ?>
+</body>
+</html>
