31be7315c793c448b8250e1d0cb2a1eaa4d752aa
Andre <andrebrreis@gmail.com>
2017-03-11 20:22:33 +0000

    add missing files

--- src/pages/landing.php
+++ src/pages/landing.php
@@ -167,15 +167,15 @@
 </section>
 
 <!-- jQuery -->
-<script src="vendor/jquery/jquery.min.js"></script>
+<!-- <script src="vendor/jquery/jquery.min.js"></script> -->
 
 <!-- Bootstrap Core JavaScript -->
-<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="../bootstrap-sass-3.3.7/assets/javascripts/bootstrap.min.js"></script>
 
 <!-- Plugin JavaScript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
-<script src="vendor/scrollreveal/scrollreveal.min.js"></script>
-<script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+<script src="../js/scrollreveal.min.js"></script>
+<script src="../js/jquery.magnific-popup.min.js"></script>
 
 <!-- Theme JavaScript -->
 <script src="../js/creative.js"></script>
