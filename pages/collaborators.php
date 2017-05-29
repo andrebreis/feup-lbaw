f817f5d328259358a711b7545b7e552abc2f7f11
Andre <andrebrreis@gmail.com>
2017-05-29 21:19:56 +0100

    fix sidebar linking issues

--- pages/collaborators.php
+++ pages/collaborators.php
@@ -4,22 +4,9 @@ include_once("../database/users.php");
 
 $id = (int)htmlspecialchars($_GET['id']);
 
-$name = getProjectField($id, 'name');
-$description = getProjectField($id, 'description');
-$completion = getProjectField($id, 'completion');
-$visible = getProjectField($id, 'visible');
-$state_name = getProjectState($id);
-$num_collaborators = getNumProjectCollaborators($id);
+include_once("project_sidebar.php");
 
 $collaborators = getProjectCollaborators($id);
-
-$smarty->assign('isCollaborator', $isCollaborator);
-$smarty->assign('name', $name);
-$smarty->assign('description', $description);
-$smarty->assign('completion', $completion);
-$smarty->assign('visible', $visible);
-$smarty->assign('state_name', $state_name);
-$smarty->assign('num_collaborators', $num_collaborators);
 
 $smarty->assign('collaborators', $collaborators);
 
--- pages/settings.php
+++ pages/settings.php
@@ -4,19 +4,6 @@ include_once("../database/users.php");
 
 $id = (int)htmlspecialchars($_GET['id']);
 
-$name = getProjectField($id, 'name');
-$description = getProjectField($id, 'description');
-$completion = getProjectField($id, 'completion');
-$visible = getProjectField($id, 'visible');
-$state_name = getProjectState($id);
-$num_collaborators = getNumProjectCollaborators($id);
-
-$smarty->assign('isCollaborator', $isCollaborator);
-$smarty->assign('name', $name);
-$smarty->assign('description', $description);
-$smarty->assign('completion', $completion);
-$smarty->assign('visible', $visible);
-$smarty->assign('state_name', $state_name);
-$smarty->assign('num_collaborators', $num_collaborators);
+include_once("project_sidebar.php");
 
 $smarty->display('../templates/common/settings.tpl');
